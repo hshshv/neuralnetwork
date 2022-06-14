3a43d4e665196b630b1c925fb1b942a4e1be9ada
hshshv <a@b>
2022-06-14 03:13:44 +0300

    in the middle of creating a POC to show that it is possible and simple to do the job of the creatures with a short genom.

--- neuron/Program.cs
+++ neuron/Program.cs
@@ -31,27 +31,7 @@ namespace neuronprog
             POCO.brian.networkDiagnos();
             Console.WriteLine("POCO RUN:");
             POCO.run(true);
-            /*
-            { 4, 4, 4}
-
-            { 5, 1, 0}
-            { 6, 1, 0}
-            { 7, 1, 0}
-            { 9, 1, 0}
-
-            { 1, 1, 1}
-            { 0, 1, 1}
-            { 2, 0.1, 1}
-            { 3, 100, 1}
-            { 3, 0, 1}
-            /*
-            msho avner = new msho(5, "hamesh");
-            msho avner2 = new msho(5, "hamesh");
-            avner2 = new msho(avner.nam, avner.text);
-            avner.nam = 4;
-            Console.WriteLine(avner2.nam + avner2.text);
-            */
-            //Console.WriteLine(mike.direction + (180 * Math.Atan((20 - mike.y) / (20 - mike.x)) / Math.PI));
+            
             /*
             Coordinator.printEvoSetting();
             creature winner = Coordinator.evolution(1000000);
@@ -64,45 +44,7 @@ namespace neuronprog
             winner.DNA.print();
             Console.WriteLine("BEST network:");
             winner.brian.networkDiagnos();
-            
-            /*
-            genome tst = new genome(3);
-            tst.addGenes(12);
-            
-            tst.genes[0].parts = new int[] { 5, 1, 1 };//num of nuerons (excluding the outputa), num of inputs, num of outputs;
-            
-            tst.genes[1].parts = new int[] { 6, neuron.Input, 0 };
-            tst.genes[2].parts = new int[] { 8, neuron.logical, 0 };
-            tst.genes[3].parts = new int[] { 9, neuron.logical, 0 };
-            tst.genes[4].parts = new int[] { 10, neuron.logical, 0 };
-            tst.genes[5].parts = new int[] { 11, neuron.logical, 0 };
-
-            tst.genes[6].parts = new int[] { 1, 50, 0 }; //connection: to neuron 2, muultiply by 80, not to the output
-            tst.genes[7].parts = new int[] { 3, 50, 0 };
-            tst.genes[8].parts = new int[] { 2, 50, 0 }; //connrction: to nueron 0, mltply 50, connected to the final layer (%2 == 1)
-            tst.genes[9].parts = new int[] { 1, 50, 0 }; //connection: to neuron 2, muultiply by 80, not to the output
-            tst.genes[10].parts = new int[] { 4, 50, 0 };
-            tst.genes[11].parts = new int[] { 0, 50, 1 };
-
-            //tst.transformInto(genome.cleanVersionOf(tst));
-            tst.clean();
-            Console.WriteLine("clean:");
-            tst.print();
-
-            /*
-            creature tstcrt = new creature(tst);
-            network tstnt = creature.getNetworkFromGenome(tst);
-
-            tstnt.neurons[0].value = 5;
-            tstnt.neurons[1].value = 8;
-
-            tstnt.networkDiagnos();
-            tstnt.Fire();
-            /*
-            tstnt.neurons[0].value = 0.1;
-            tstnt.neurons[1].value = 0.6;
-            tstnt.Fire();
-            */
+           */ 
         }
     }
 }
